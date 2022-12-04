0a1ae992e2109b24054abd32280c4daa2dd809c0
Ryan <ryan.cfplus@gmail.com>
2022-12-03 19:40:41 -0500

    Convert multithreading to multiprocessing in qt app

--- online_video_prediction_qt.py
+++ online_video_prediction_qt.py
@@ -28,6 +28,7 @@ from numpy.lib.function_base import copy
 import threading
 from pyqtgraph import PlotWidget, plot
 import pyqtgraph
+from multiprocessing import Process, Pipe, Array, Lock
 
 from data_loader import SingleVideoIter
 from feature_extractor import to_segments
@@ -61,51 +62,73 @@ def get_args() -> argparse.Namespace:
 
     return parser.parse_args()
 
-clip_length = 16
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def inference_process(pipe: Pipe, feature_extractor, ad_model, feature_method, forced_delay):
+    print('loading models...')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    anomaly_detector, feature_extractor = load_models(
+        feature_extractor,
+        ad_model,
+        features_method=feature_method,
+        device=device,
+    )
 
-global_edit_lock = threading.Condition()
-frame_buffer = []
-frame_times = []
-frame_number = -1
-pred_x = []
-pred_y = []
-processing_fps = 0
-close_processing_thread = False
+    transforms = build_transforms(mode=feature_method)
 
-def processing_thread(transforms, feature_extractor, anomaly_detector):
-    global clip_length
-    global device
-    global frame_buffer
-    global frame_number
-    global pred_x
-    global pred_y
-    global processing_fps
-    global close_processing_thread
+    # Warmup the models. If this is ignored, the initial part of the video 
+    # will not be processed as the warmup occurs there. Not sure why the 
+    # models need to warm up.
+    print('warming up models...')
+    feature_extractor(torch.zeros((1, 3, 16, 112, 112)).to(device))
+    anomaly_detector(torch.zeros((1, 4096)).to(device))
+
+    print('inference process ready...')
+    pipe.send(('status', 'ready'))
+
+    CLIP_LENGTH = 16
+
+    frame_buffer = []
+    frame_buffer_numbers = []
+    frame_buffer_dirty = False
+    cap = None
 
     last_frame_number = -1
     with torch.no_grad():
         while True:
-            # clip_processing_start = time.perf_counter()
-            if close_processing_thread:
-                break
-            with global_edit_lock:
-                if last_frame_number == frame_number or len(frame_buffer) < clip_length:
-                    continue
+            while pipe.poll():
+                message = pipe.recv()
+                if message[0] == 'open':
+                    frame_buffer = []
+                    frame_buffer_numbers = []
+                    frame_buffer_dirty = False
+
+                    filepath = message[1]
+                    cap = cv2.VideoCapture(filepath)
+                elif message[0] == 'frame':
+                    frame_number = message[1]
+                    assert len(frame_buffer_numbers) == 0 or frame_number == frame_buffer_numbers[-1] + 1
+                    frame_buffer_numbers.append(frame_number)
+                    frame_buffer_numbers = frame_buffer_numbers[-CLIP_LENGTH:]
+                    frame = cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2RGB)
+                    frame_buffer.append(frame)
+                    frame_buffer = frame_buffer[-CLIP_LENGTH:]
+                    frame_buffer_dirty = True
             
-            with global_edit_lock:
-                last_frame_number = frame_number
-                clip = torch.tensor(copy(frame_buffer[-clip_length:]))
-                clip_start_time = frame_times[-clip_length]
+            if not frame_buffer_dirty:
+                continue
+
+            if len(frame_buffer) < CLIP_LENGTH:
+                continue
+            
+            last_frame_number = frame_buffer_numbers[-1]
+            clip = torch.tensor(copy(frame_buffer[-CLIP_LENGTH:]))
             clip = transforms(clip).unsqueeze(0)
             outputs = feature_extractor(clip.to(device)).detach().cpu().numpy()
             outputs = to_segments(outputs, 1)
             new_pred = anomaly_detector(torch.tensor(outputs).to(device))
-            with global_edit_lock:
-                clip_processing_time = time.perf_counter() - clip_start_time
-                pred_x.append(last_frame_number)
-                pred_y.append(new_pred.item())
-                processing_fps = clip_length / clip_processing_time
+            accurate_sleep(forced_delay)
+            frame_buffer_dirty = False
+            
+            pipe.send(('inference', (last_frame_number - 16 + 1, last_frame_number, new_pred.item())))
 
 def accurate_sleep(seconds):
     wait_until = time.perf_counter() + seconds
@@ -120,10 +143,16 @@ class Window(QWidget):
     https://codeloop.org/python-how-to-create-media-player-in-pyqt5/
     """
 
-    def __init__(self) -> None:
+    def __init__(self, pipe) -> None:
         super().__init__()
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        self.frame_number = 0
+        self.frame_times = []
+        self.pred_x_buffer = []
+        self.pred_y_buffer = []
+        self.pipe = pipe
 
         self.setWindowTitle("Anomaly Media Player")
         self.setGeometry(350, 100, 700, 500)
@@ -165,54 +194,60 @@ class Window(QWidget):
         self.setLayout(gridLayout)
 
     def open_file(self):
-        global frame_buffer
-        global frame_times
-        global frame_number
-        global pred_x
-        global pred_y
-
         filename, _ = QFileDialog.getOpenFileName(self, "Open Video")
         if filename != "":
-            pred_x = []
-            pred_y = []
-            frame_buffer = []
-            frame_times = []
-            frame_number = -1
+            self.frame_number = -1
+            self.frame_times = []
+            self.pred_x_buffer = []
+            self.pred_y_buffer = []
+
+            while self.pipe.poll():
+                self.pipe.recv() # discard leftover messages from last video
+            self.pipe.send(('open', filename))
 
             self.play_video(filename)
 
     def play_video(self, filepath):
-        global frame_buffer
-        global frame_times
-        global frame_number
-        global pred_x
-        global pred_y
-
         cap = cv2.VideoCapture(filepath)
         fps = int(cap.get(cv2.CAP_PROP_FPS))
         spf = 1 / fps
         last_frame_time = -1
 
+        inference_fps = None
+
         while cap.isOpened():
             ret, frame = cap.read()
             if not ret:
                 return
+
+            if self.pipe.poll():
+                message = self.pipe.recv()
+                if message[0] == 'inference':
+                    start_frame, end_frame = message[1][0], message[1][1]
+                    prediction = message[1][2]
+                    self.pred_x_buffer.append(end_frame)
+                    self.pred_x_buffer = self.pred_x_buffer[-64:]
+                    self.pred_y_buffer.append(prediction)
+                    self.pred_y_buffer = self.pred_y_buffer[-64:]
+                    initial_frame_time = self.frame_times[start_frame]
+                    inference_fps = (end_frame - start_frame + 1) / (time.perf_counter() - initial_frame_time)
             
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             pxmap = QPixmap.fromImage(QImage(frame, frame.shape[1],frame.shape[0],frame.strides[0],QImage.Format_RGB888))
+            self.frame_number += 1
+            self.pipe.send(('frame', self.frame_number))
+            
             delta = time.perf_counter() - last_frame_time
             accurate_sleep(max(0, spf - delta))
-            with global_edit_lock:
-                frame_number += 1
-                frame_buffer.append(frame)
-                last_frame_time = time.perf_counter()
-                frame_times.append(last_frame_time)
-                frame_buffer = frame_buffer[-clip_length:]
-                self.label.setPixmap(pxmap)
-                self.fps_label.setText(f' {processing_fps:.0f} fps')
+            self.label.setPixmap(pxmap)
+            last_frame_time = time.perf_counter()
+            self.frame_times.append(last_frame_time)
 
-                if frame_number % 10 == 0:
-                    self.graphData.setData(pred_x[-64:], pred_y[-64:])
+            if inference_fps is not None:
+                self.fps_label.setText(f' {inference_fps:.0f} fps')
+
+            if self.frame_number % 10 == 0:
+                self.graphData.setData(self.pred_x_buffer, self.pred_y_buffer)
 
             QApplication.processEvents()
         cap.release()
@@ -220,29 +255,20 @@ class Window(QWidget):
 if __name__ == "__main__":
     args = get_args()
 
-    print('loading models...')
-    anomaly_detector, feature_extractor = load_models(
-        args.feature_extractor,
-        args.ad_model,
-        features_method=args.feature_method,
-        device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
-    )
+    print('starting model process...')
+    parent_conn, child_conn = Pipe()
+    p = Process(target=inference_process, args=(child_conn, args.feature_extractor, args.ad_model, args.feature_method, 1))
+    p.start()
 
-    transforms = build_transforms(mode=args.feature_method)
-
-    print('warming up models...')
-    # Warmup the models. If this is ignored, the initial part of the video 
-    # will not be processed as the warmup occurs there. Not sure why the 
-    # models need to warm up.
-    feature_extractor(torch.zeros((1, 3, 16, 112, 112)).to(device))
-    anomaly_detector(torch.zeros((1, 4096)).to(device))
-
-    print('starting processing thread...')
-    threading.Thread(target=processing_thread, args=(transforms, feature_extractor, anomaly_detector)).start()
+    await_status_message = parent_conn.recv()
+    if await_status_message[0] != 'status':
+        raise Exception(f'Expected status message, got {await_status_message[0]}')
+    if await_status_message[1] != 'ready':
+        raise Exception(f'Expected inference process to be "ready", got {await_status_message[1]}')
 
     print('starting app...')
     app = QApplication(sys.argv)
-    window = Window()
+    window = Window(parent_conn)
     rc = app.exec_()
-    close_processing_thread = True
+    p.join()
     sys.exit(rc)
